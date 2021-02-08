b1497c50f38e786762d22fcaf1af5d3ee68e0790
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-02-08 11:54:29 -0500

    Show maxquant-tools version in install script.

--- install/install.sh
+++ install/install.sh
@@ -14,6 +14,6 @@ then
     echo "Creating python virtual environment at $VENV"
     python3 -m venv "$VENV"
 fi
-echo "Updating python libraries"
+echo "Updating python libraries using $VERSION"
 "$VENV"/bin/pip uninstall -y MaxquantTools
 "$VENV"/bin/pip install git+file://"$MAXQUANT_TOOLS"@"$VERSION"
