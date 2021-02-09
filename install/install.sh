8a8ab438a44439fc9c9d8479ac1d993f0261dcee
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-02-08 19:37:15 -0500

    Skip reading version on 'install.sh clean'.

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VENV="$MAXQUANT_TOOLS"/venv
-VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 
 if [ "$1" == "clean" ]
 then
@@ -14,6 +13,7 @@ then
     echo "Creating python virtual environment at $VENV"
     python3 -m venv "$VENV"
 fi
+VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
 "$VENV"/bin/pip uninstall -y MaxquantTools
 "$VENV"/bin/pip install git+file://"$MAXQUANT_TOOLS"@"$VERSION"
