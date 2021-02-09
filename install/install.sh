5de32bbd3109f1116aa59e9a82413b41e16c8481
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-02-08 19:37:59 -0500

    Fixes shebang in scripts created by pip.

--- install/install.sh
+++ install/install.sh
@@ -17,3 +17,4 @@ VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
 "$VENV"/bin/pip uninstall -y MaxquantTools
 "$VENV"/bin/pip install git+file://"$MAXQUANT_TOOLS"@"$VERSION"
+find "$VENV" -type f -perm 750 -exec sed -i "1 s|^.*$|#!/usr/bin/env python|g" {} \;
