52880506e15afe7353251ed40d8d8148903f8a91
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-02-08 19:38:25 -0500

    Adds potential fix for 's' permission flag.

--- install/install.sh
+++ install/install.sh
@@ -12,6 +12,7 @@ if [ ! -d "$VENV" ]
 then
     echo "Creating python virtual environment at $VENV"
     python3 -m venv "$VENV"
+#    find "$VENV" -type d -exec chmod ug-s {} \; # Fixes execute permission.
 fi
 VERSION=$(git --git-dir="$MAXQUANT_TOOLS"/.git rev-parse --abbrev-ref HEAD)
 echo "Updating python libraries using $VERSION"
