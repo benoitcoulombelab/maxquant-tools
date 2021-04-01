95fa0822a69a195fee9aeb20ce2b0fb4e5f0d587
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-01 09:08:17 -0400

    Updates mono and python for StdEnv/2020 on compute canada.

--- install/maxquant.lua
+++ install/maxquant.lua
@@ -6,19 +6,17 @@ This module sets the following environment variables:
     MAXQUANT_BASE: directory containing MaxQuant programs and bash scripts
 
 This module loads the following modules and their requirements:
-    - gcc/7.3.0
-    - mono/5.16.0.179
-    - python/3.7.4
+    - mono/6.12.0.122
+    - python/3.8.2
 ]])
 
-whatis("Version: 1.0.0")
+whatis("Version: 1.0.1")
 whatis("Keywords: MaxQuant, Utility")
 whatis("URL: https://maxquant.net")
 whatis("Description: MaxQuant is a quantitative proteomics software package designed for analyzing large mass-spectrometric data sets.")
 
-always_load("gcc/7.3.0")
-always_load("mono/5.16.0.179")
-always_load("python/3.7.4")
+always_load("mono/6.12.0.122")
+always_load("python/3.8.2")
 
 local home = os.getenv("HOME") or ""
 local project = pathJoin(home, "projects/def-coulomb")
