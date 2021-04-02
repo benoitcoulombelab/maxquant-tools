5827d4d3e3516ee8f95b1b6a997836422f0f9573
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-04-02 16:26:59 -0400

    Moves 'module use' command to .coulomb_addons file.

--- install/configure.sh
+++ install/configure.sh
@@ -5,6 +5,7 @@ then
     exit 1
 fi
 
+# Set user's email.
 if grep -Fq "JOB_MAIL" ~/.bash_profile ; then
   echo "Email address environment variables present in .bash_profile"
 else
@@ -13,15 +14,29 @@ else
   echo "" >> ~/.bash_profile
 fi
 
-if grep -Fq "COULOMB_MODULES_DIR" ~/.bash_profile ; then
-  echo "Modules directory present in .bash_profile"
-else
-  echo "Adding modules directory to .bash_profile"
-  echo "## Coulombe Lab Modules ##" >> ~/.bash_profile
-  echo "COULOMB_MODULES_DIR=~/projects/def-coulomb/modules" >> ~/.bash_profile
-  echo 'if [ -d "$COULOMB_MODULES_DIR" ]; then' >> ~/.bash_profile
-  echo '  module use $COULOMB_MODULES_DIR' >> ~/.bash_profile
+# Remove direct configuration of coulomb modules, if present.
+if grep -Fq "COULOMB_MODULES_DIR=" ~/.bash_profile ; then
+  echo "Removing coulomb modules from .bash_profile"
+  INDEX=$(grep -n "COULOMB_MODULES_DIR=" ~/.bash_profile | cut -d: -f1)
+  sed -i "$((INDEX-1)),$((INDEX+4))d" ~/.bash_profile
+fi
+
+# Source .coulomb_addons file on login.
+if ! grep -Fq "source .coulomb_addons" ~/.bash_profile ; then
+  echo "Adding coulomb addons"
+  echo 'if [ -f .coulomb_addons ]; then' >> ~/.bash_profile
+  echo '  source .coulomb_addons' >> ~/.bash_profile
   echo 'fi' >> ~/.bash_profile
   echo "" >> ~/.bash_profile
 fi
 
+# Create .coulomb_addons file to alow loading of coulomb modules.
+if [ -f ~/.coulomb_addons ] ; then
+  rm ~/.coulomb_addons
+fi
+echo "## Coulombe Lab Modules ##" >> ~/.coulomb_addons
+echo "COULOMB_MODULES_DIR=~/projects/def-coulomb/modules" >> ~/.coulomb_addons
+echo 'if [ -d "$COULOMB_MODULES_DIR" ]; then' >> ~/.coulomb_addons
+echo '  module use $COULOMB_MODULES_DIR' >> ~/.coulomb_addons
+echo 'fi' >> ~/.coulomb_addons
+echo "" >> ~/.coulomb_addons
