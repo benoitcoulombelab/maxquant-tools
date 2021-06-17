9bcf741e419ef9779dae121a7e7f869cda5faf8b
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-06-17 10:13:44 -0400

    Version is now 1.0-SNAPSHOT.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MaxquantTools',
-    version='0.1',
+    version='1.0-SNAPSHOT',
     packages=find_packages(),
     author='Christian Poitras',
     author_email='christian.poitras@ircm.qc.ca',
